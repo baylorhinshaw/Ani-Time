33ac460350fe1a9eb3a962da4100943195cb5624
Baylor <baylorhinshaw21@gmail.com>
2022-02-07 21:19:16 -0500

    added to query user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,6 +5,15 @@ export const QUERY_USER = gql`
     user {
       firstName
       lastName
+      email
+      password
+      savedAnimes {
+        mal_id 
+        titleEnglish
+        titleJapanese
+        image
+        score
+      }
     }
   }
 `;

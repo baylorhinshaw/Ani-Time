3d90540c722df8da8d015dd32058c11e12834d1b
Bora Lee <bbora1119@gmail.com>
2022-02-09 20:30:21 -0500

    add $ to the server/muation file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -32,3 +32,31 @@ export const ADD_USER = gql`
   }
 `;
 
+
+export const SAVE_ANIME = gql`
+mutation saveAnime($titleEnglish: String, $titleJapanese: String, $score: Int!, $mal_id: Int, $genres: [String], $image: String) {
+  saveAnime(titleEnglish: $titleEnglish, titleJapanese: $titleJapanese, score: $score, mal_id: $mal_id, genres: $genres, image: $image) {
+    _id
+    firstname
+    lastname
+    email
+    savedAnimes {
+      mal_id
+    }
+  }
+}
+`;
+
+export const REMOVE_ANIME = gql`
+mutation removeAnime($mal_id: Int) {
+  removeAnime(mal_id: $mal_id) {
+    _id
+    firstname
+    lastname
+    email
+    savedAnimes {
+      mal_id
+    }
+  }
+}
+`;
